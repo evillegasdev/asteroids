e84729ac1dac398319a2ee1edf2b73536f0648db
evillegasdev <efrain@villegas.dev>
2025-09-08 23:21:08 -0600

    CH:2 L5 added circleshape hitbox and player class to render player on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,13 +10,21 @@ def main():
     pygame_init = pygame.init()
     if pygame_init[1] != 0:
         print(f"Initialized {pygame_init[0]}/{pygame_init[0] + pygame_init[1]} pygame modules")
+    clock_object = pygame.time.Clock()
+    dt = 0 # time delta since .tick() was last called
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2) 
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock_object.tick(60) / 1000
 
 
 if __name__ == "__main__":
