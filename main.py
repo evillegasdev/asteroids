b47d4d919ac6feb98af18ffc1d17e24a5230bb3d
evillegasdev <efrain@villegas.dev>
2025-09-12 18:42:14 -0600

    added Asteroid and AsteroidField classes and updated game logic to spawn asteroids

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ import pygame
 from pygame.locals import *
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\n"
           f"Screen height: {SCREEN_HEIGHT}")
+
     pygame_init = pygame.init()
     if pygame_init[1] != 0:
         print(f"Initialized {pygame_init[0]}/{pygame_init[0] + pygame_init[1]} pygame modules")
+
     clock_object = pygame.time.Clock()
     dt = 0 # initial time delta since .tick() was last called
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create groups and add classes to groups via 'containers' attribute
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    all_asteroids = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (all_asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    # initialize player and asteroidfield object
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
 
     while True:
